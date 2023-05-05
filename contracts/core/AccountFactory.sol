eec5eebf7ac7605bc40d3285535d41b6584cc890
0xmikko <26343374+0xmikko@users.noreply.github.com>
2023-05-05 19:54:27 +0200

    feat: new reusable account factory

--- contracts/core/AccountFactory.sol
+++ contracts/core/AccountFactory.sol
@@ -9,17 +9,26 @@ import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
 import {CreditAccount} from "../credit/CreditAccount.sol";
 import {ACLTrait} from "../traits/ACLTrait.sol";
 
-import {IAccountFactory} from "../interfaces/IAccountFactory.sol";
+import {IAccountFactory, TakeAccountAction} from "../interfaces/IAccountFactory.sol";
 
 // EXCEPTIONS
 import "../interfaces/IExceptions.sol";
 
 import "forge-std/console.sol";
 
+struct CreditManagerFactory {
+    address masterCreditAccount;
+    uint32 head;
+    uint32 tail;
+    uint16 minUsedInQueue;
+}
+
 /// @title Disposable credit accounts factory
 contract AccountFactoryV2 is IAccountFactory, ACLTrait, ContractsRegisterTrait {
     /// @dev Address of master credit account for cloning
-    mapping(address => address) public masterCreditAccounts;
+    mapping(address => CreditManagerFactory) public masterCreditAccounts;
+
+    mapping(address => address[]) public usedCreditAccounts;
 
     /// @dev Contract version
     uint256 public constant version = 3_00;
@@ -31,38 +40,57 @@ contract AccountFactoryV2 is IAccountFactory, ACLTrait, ContractsRegisterTrait {
 
     /// @dev Provides a new credit account to a Credit Manager
     /// @return creditAccount Address of credit account
-    function takeCreditAccount(uint256, uint256) external override returns (address creditAccount) {
-        address masterCreditAccount = _getMasterCreditAccountOrRevert();
-        // Create a new credit account if there are none in stock
-        creditAccount = Clones.clone(masterCreditAccount); // T:[AF-2]
+    function takeCreditAccount(uint256 deployAction, uint256) external override returns (address creditAccount) {
+        CreditManagerFactory storage cmf = masterCreditAccounts[msg.sender];
+        address masterCreditAccount = cmf.masterCreditAccount;
+
+        if (masterCreditAccount == address(0)) {
+            revert CallerNotCreditManagerException();
+        }
+        uint256 totalUsed = cmf.tail - cmf.head;
+        if (totalUsed < cmf.minUsedInQueue || deployAction == uint256(TakeAccountAction.DEPLOY_NEW_ONE)) {
+            // Create a new credit account if there are none in stock
+            creditAccount = Clones.clone(masterCreditAccount); // T:[AF-2]
+            emit DeployCreditAccount(creditAccount);
+        } else {
+            creditAccount = usedCreditAccounts[msg.sender][cmf.head];
+            ++cmf.head;
+            emit ReuseCreditAccount(creditAccount);
+        }
 
         // emit InitializeCreditAccount(result, msg.sender); // T:[AF-5]
     }
 
     function returnCreditAccount(address usedAccount) external override {
-        // Do nothing for disposable CA
+        CreditManagerFactory storage cmf = masterCreditAccounts[msg.sender];
+
+        if (cmf.masterCreditAccount == address(0)) {
+            revert CallerNotCreditManagerException();
+        }
+
+        usedCreditAccounts[msg.sender][cmf.tail] = usedAccount;
+        ++cmf.tail;
+        emit ReturnCreditAccount(usedAccount);
     }
 
     // CONFIGURATION
 
-    function addCreditManager(address creditManager)
+    function addCreditManager(address creditManager, uint16 minUsedInQueue)
         external
         configuratorOnly
         registeredCreditManagerOnly(creditManager)
     {
-        if (masterCreditAccounts[creditManager] != address(0)) {
+        if (masterCreditAccounts[creditManager].masterCreditAccount != address(0)) {
             revert MasterCreditAccountAlreadyDeployed();
         }
 
-        masterCreditAccounts[creditManager] = address(new CreditAccount(creditManager));
-    }
+        masterCreditAccounts[creditManager] = CreditManagerFactory({
+            masterCreditAccount: address(new CreditAccount(creditManager)),
+            head: 0,
+            tail: 0,
+            minUsedInQueue: minUsedInQueue
+        });
 
-    function _getMasterCreditAccountOrRevert() internal view returns (address masterCA) {
-        masterCA = masterCreditAccounts[msg.sender];
-        console.log(msg.sender);
-        console.log(masterCA);
-        if (masterCA == address(0)) {
-            revert CallerNotCreditManagerException();
-        }
+        emit AddCreditManager(creditManager);
     }
 }
