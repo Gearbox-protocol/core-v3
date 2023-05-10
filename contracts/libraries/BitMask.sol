9e66a8144816435b2e2cb99fec57de6644fe6546
Mikael <26343374+0xmikko@users.noreply.github.com>
2023-05-10 15:22:52 +0200

    fix: bit mask tests added

--- contracts/libraries/BitMask.sol
+++ contracts/libraries/BitMask.sol
@@ -10,20 +10,21 @@ uint256 constant UNDERLYING_TOKEN_MASK = 1;
 /// @title BitMask library
 library BitMask {
     function calcIndex(uint256 mask) internal pure returns (uint8) {
-        if (mask == 0) revert IncorrectParameterException();
-        uint16 lb = 0;
-        uint16 ub = 256;
-        uint16 mid = 128;
+        if (mask == 0) revert IncorrectParameterException(); // U:[BM-1]
+        uint16 lb = 0; // U:[BM-2]
+        uint16 ub = 256; // U:[BM-2]
+        uint16 mid = 128; // U:[BM-2]
 
         unchecked {
-            while (1 << mid & mask == 0) {
-                if (1 << mid > mask) ub = mid;
-                else lb = mid;
-                mid = (lb + ub) / 2;
+            while (true) {
+                uint256 newMask = 1 << mid;
+                if (newMask & mask != 0) return uint8(mid); // U:[BM-2]
+                if (1 << mid > mask) ub = mid; // U:[BM-2]
+
+                else lb = mid; // U:[BM-2]
+                mid = (lb + ub) >> 1; // U:[BM-2]
             }
         }
-
-        return uint8(mid);
     }
 
     /// @dev Calculates the number of `1` bits
@@ -31,18 +32,18 @@ library BitMask {
     function calcEnabledTokens(uint256 enabledTokensMask) internal pure returns (uint256 totalTokensEnabled) {
         unchecked {
             while (enabledTokensMask > 0) {
-                totalTokensEnabled += enabledTokensMask & 1;
-                enabledTokensMask >>= 1;
+                totalTokensEnabled += enabledTokensMask & 1; // U:[BM-3]
+                enabledTokensMask >>= 1; // U:[BM-3]
             }
         }
     }
 
     function enable(uint256 enabledTokenMask, uint256 bitsToEnable) internal pure returns (uint256) {
-        return enabledTokenMask | bitsToEnable;
+        return enabledTokenMask | bitsToEnable; // U:[BM-4]
     }
 
     function disable(uint256 enabledTokenMask, uint256 bitsToDisable) internal pure returns (uint256) {
-        return enabledTokenMask & ~bitsToDisable;
+        return enabledTokenMask & ~bitsToDisable; // U:[BM-4]
     }
 
     function enableDisable(uint256 enabledTokensMask, uint256 bitsToEnable, uint256 bitsToDisable)
@@ -50,7 +51,7 @@ library BitMask {
         pure
         returns (uint256)
     {
-        return enabledTokensMask | bitsToEnable & (~bitsToDisable);
+        return (enabledTokensMask | bitsToEnable) & (~bitsToDisable); // U:[BM-5]
     }
 
     function enable(uint256 enabledTokenMask, uint256 bitsToEnable, uint256 invertedSkipMask)
@@ -58,7 +59,7 @@ library BitMask {
         pure
         returns (uint256)
     {
-        return enabledTokenMask | (bitsToEnable & invertedSkipMask);
+        return enabledTokenMask | (bitsToEnable & invertedSkipMask); // U:[BM-6]
     }
 
     function disable(uint256 enabledTokenMask, uint256 bitsToDisable, uint256 invertedSkipMask)
@@ -66,7 +67,7 @@ library BitMask {
         pure
         returns (uint256)
     {
-        return enabledTokenMask & (~(bitsToDisable & invertedSkipMask));
+        return enabledTokenMask & (~(bitsToDisable & invertedSkipMask)); // U:[BM-6]
     }
 
     function enableDisable(
@@ -75,6 +76,6 @@ library BitMask {
         uint256 bitsToDisable,
         uint256 invertedSkipMask
     ) internal pure returns (uint256) {
-        return (enabledTokensMask | (bitsToEnable & invertedSkipMask)) & (~(bitsToDisable & invertedSkipMask));
+        return (enabledTokensMask | (bitsToEnable & invertedSkipMask)) & (~(bitsToDisable & invertedSkipMask)); // U:[BM-7]
     }
 }
