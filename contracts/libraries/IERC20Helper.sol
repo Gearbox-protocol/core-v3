b47d662f1d55c20673b6a40bff9fa6883b49865f
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-05-27 16:39:52 +0300

    test: erc20 helper unit tests

--- contracts/libraries/IERC20Helper.sol
+++ contracts/libraries/IERC20Helper.sol
@@ -1,28 +1,34 @@
 // SPDX-License-Identifier: BUSL-1.1
 // Gearbox Protocol. Generalized leverage for DeFi protocols
-// (c) Gearbox Holdings, 2022
+// (c) Gearbox Holdings, 2023
 pragma solidity ^0.8.17;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-/// @title IERC20HelperTrait
-/// @notice Saves size by providing internal call for balanceOf
+/// @title ERC20 helper library
 library IERC20Helper {
+    /// @dev Returns `holder`'s balance of `token`
+    ///      Can be used to reduce the size of a contract that makes multiple external
+    ///      `balanceOf` calls by making them internal (yes, it really works)
     function balanceOf(address token, address holder) internal view returns (uint256) {
         return IERC20(token).balanceOf(holder);
     }
 
+    /// @dev Same as OpenZeppelin's `safeTransfer`, but, instead of reverting, returns `false` when transfer fails
     function unsafeTransfer(IERC20 token, address to, uint256 amount) internal returns (bool success) {
-        return _unsafeCall(address(token), abi.encodeCall(IERC20.transfer, (to, amount)));
+        return _unsafeCall(address(token), abi.encodeCall(IERC20.transfer, (to, amount))); // U:[EH-1]
     }
 
+    /// @dev Same as OpenZeppelin's `safeTransferFrom`, but, instead of reverting, returns `false` when transfer fails
     function unsafeTransferFrom(IERC20 token, address from, address to, uint256 amount)
         internal
         returns (bool success)
     {
-        return _unsafeCall(address(token), abi.encodeCall(IERC20.transferFrom, (from, to, amount)));
+        return _unsafeCall(address(token), abi.encodeCall(IERC20.transferFrom, (from, to, amount))); // U:[EH-2]
     }
 
+    /// @dev Executes call to a function that returns either boolean value indicating call success or nothing
+    ///      Returns `true` if call is successful (didn't revert, didn't return false) or `false` otherwise
     function _unsafeCall(address addr, bytes memory data) private returns (bool) {
         (bool success, bytes memory returndata) = addr.call(data);
         return success && (returndata.length == 0 || abi.decode(returndata, (bool)));
