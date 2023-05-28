6d8652cd90726117db6f50d465619a1bd4e5b270
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-05-28 15:07:41 +0300

    feat: pool improvements
    * rename stored -> LU
    * add name and symbol prefix to constructor
    * USDT pool code cleanup

--- contracts/pool/PoolV3_USDT.sol
+++ contracts/pool/PoolV3_USDT.sol
@@ -5,24 +5,30 @@ pragma solidity ^0.8.17;
 
 import {PoolV3} from "./PoolV3.sol";
 import {USDT_Transfer} from "../traits/USDT_Transfer.sol";
-import {IPoolV3} from "../interfaces/IPoolV3.sol";
 
-/// @title Core pool contract compatible with ERC4626
-/// @notice Implements pool & dieselUSDT_Transferogic
-
+/// @title Pool V3 USDT
+/// @notice Pool implementation for USDT which has optional transfer fee
 contract PoolV3_USDT is PoolV3, USDT_Transfer {
     constructor(
         address addressProvider_,
         address underlyingToken_,
         address interestRateModel_,
         uint256 totalDebtLimit_,
-        bool supportsQuotas_
+        bool supportsQuotas_,
+        string memory namePrefix_,
+        string memory symbolPrefix_
     )
-        PoolV3(addressProvider_, underlyingToken_, interestRateModel_, totalDebtLimit_, supportsQuotas_)
+        PoolV3(
+            addressProvider_,
+            underlyingToken_,
+            interestRateModel_,
+            totalDebtLimit_,
+            supportsQuotas_,
+            namePrefix_,
+            symbolPrefix_
+        )
         USDT_Transfer(underlyingToken_)
-    {
-        // Additional check that receiver is not address(0)
-    }
+    {}
 
     function _amountWithFee(uint256 amount) internal view override returns (uint256) {
         return _amountUSDTWithFee(amount);
