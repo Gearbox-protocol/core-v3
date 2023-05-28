6d3ed8afbd233640441665e16f2096544cfc34e1
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-05-29 02:10:52 +0300

    fix: traits cleanup
    * inherit ACLNonReentrantTrait from ReentrancyGuardTrait
    * interfaces -> addresses in contract state
    * add NatSpec

--- contracts/traits/ACLTrait.sol
+++ contracts/traits/ACLTrait.sol
@@ -1,31 +1,37 @@
 // SPDX-License-Identifier: BUSL-1.1
 // Gearbox Protocol. Generalized leverage for DeFi protocols
-// (c) Gearbox Holdings, 2022
+// (c) Gearbox Holdings, 2023
 pragma solidity ^0.8.17;
 
-import {AddressProvider} from "@gearbox-protocol/core-v2/contracts/core/AddressProvider.sol";
 import {IACL} from "@gearbox-protocol/core-v2/contracts/interfaces/IACL.sol";
-import "../interfaces/IExceptions.sol";
+
+import {AP_ACL, IAddressProviderV3, NO_VERSION_CONTROL} from "../interfaces/IAddressProviderV3.sol";
+import {CallerNotConfiguratorException} from "../interfaces/IExceptions.sol";
 
 import {SanityCheckTrait} from "./SanityCheckTrait.sol";
 
-/// @title ACL Trait
-/// @notice Utility class for ACL consumers
+/// @title ACL trait
+/// @notice Utility class for ACL (access-control list) consumers
 abstract contract ACLTrait is SanityCheckTrait {
-    // ACL contract to check rights
-    IACL public immutable _acl;
+    /// @notice ACL contract address
+    address public immutable acl;
 
-    /// @dev constructor
-    /// @param addressProvider Address of address repository
+    /// @notice Constructor
+    /// @param addressProvider Address provider contract address
     constructor(address addressProvider) nonZeroAddress(addressProvider) {
-        _acl = IACL(AddressProvider(addressProvider).getACL());
+        acl = IAddressProviderV3(addressProvider).getAddressOrRevert(AP_ACL, NO_VERSION_CONTROL);
     }
 
-    /// @dev  Reverts if msg.sender is not configurator
+    /// @dev Ensures that function caller has configurator role
     modifier configuratorOnly() {
-        if (!_acl.isConfigurator(msg.sender)) {
+        if (!_isConfigurator({account: msg.sender})) {
             revert CallerNotConfiguratorException();
         }
         _;
+    }
+
+    /// @dev Checks whether given account has configurator role
+    function _isConfigurator(address account) internal view returns (bool) {
+        return IACL(acl).isConfigurator(account);
     }
 }
--- contracts/traits/ReentrancyGuardTrait.sol
+++ contracts/traits/ReentrancyGuardTrait.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: BUSL-1.1
 // Gearbox Protocol. Generalized leverage for DeFi protocols
-// (c) Gearbox Holdings, 2022
+// (c) Gearbox Holdings, 2023
 pragma solidity ^0.8.17;
 
 uint8 constant NOT_ENTERED = 1;
 uint8 constant ENTERED = 2;
 
-/// @title ReentrancyGuardTrait
+/// @title Reentrancy guard trait
+/// @notice Same as OpenZeppelin's `ReentrancyGuard` but only uses 1 byte of storage instead of 32
 abstract contract ReentrancyGuardTrait {
     uint8 internal _reentrancyStatus = NOT_ENTERED;
 
@@ -15,7 +16,6 @@ abstract contract ReentrancyGuardTrait {
     /// function is not supported. It is possible to prevent this from happening
     /// by making the `nonReentrant` function external, and making it call a
     /// `private` function that does the actual work.
-    ///
     modifier nonReentrant() {
         // On the first call to nonReentrant, _notEntered will be true
         require(_reentrancyStatus != ENTERED, "ReentrancyGuard: reentrant call");
