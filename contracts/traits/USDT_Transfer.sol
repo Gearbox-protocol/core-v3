087701004521e343c8285fdebf165de5278867ae
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-06-08 16:04:42 +0300

    perf: optimize usdt transfer
    * external calls are wrapped in internal functions (reduces size)
    * return amount if fee rate is 0 (saves one external call)
    * fix comments

--- contracts/traits/USDT_Transfer.sol
+++ contracts/traits/USDT_Transfer.sol
@@ -7,30 +7,42 @@ import {USDTFees} from "../libraries/USDTFees.sol";
 
 interface IUSDT {
     function basisPointsRate() external view returns (uint256);
-
     function maximumFee() external view returns (uint256);
 }
 
+/// @title USDT transfer
+/// @notice Trait that allows to calculate amounts adjusted for transfer fees
 contract USDT_Transfer {
     using USDTFees for uint256;
 
+    /// @dev USDT token address
     address private immutable usdt;
 
     constructor(address _usdt) {
         usdt = _usdt;
     }
 
-    /// @dev Computes how much usdt you should send to get exact amount on destination account
+    /// @dev Computes amount of USDT that should be sent to receive `amount`
     function _amountUSDTWithFee(uint256 amount) internal view virtual returns (uint256) {
-        uint256 basisPointsRate = IUSDT(usdt).basisPointsRate(); // U:[UTT_01]
-        uint256 maximumFee = IUSDT(usdt).maximumFee(); // U:[UTT_01]
-        return amount.amountUSDTWithFee({basisPointsRate: basisPointsRate, maximumFee: maximumFee}); // U:[UTT_01]
+        uint256 basisPointsRate = _basisPointsRate(); // U:[UTT_01]
+        if (basisPointsRate == 0) return amount;
+        return amount.amountUSDTWithFee({basisPointsRate: basisPointsRate, maximumFee: _maximumFee()}); // U:[UTT_01]
     }
 
-    /// @dev Computes how much usdt you should send to get exact amount on destination account
+    /// @dev Computes amount of USDT that would be received if `amount` is sent
     function _amountUSDTMinusFee(uint256 amount) internal view virtual returns (uint256) {
-        uint256 basisPointsRate = IUSDT(usdt).basisPointsRate(); // U:[UTT_01]
-        uint256 maximumFee = IUSDT(usdt).maximumFee(); // U:[UTT_01]
-        return amount.amountUSDTMinusFee({basisPointsRate: basisPointsRate, maximumFee: maximumFee}); // U:[UTT_01]
+        uint256 basisPointsRate = _basisPointsRate(); // U:[UTT_01]
+        if (basisPointsRate == 0) return amount;
+        return amount.amountUSDTMinusFee({basisPointsRate: basisPointsRate, maximumFee: _maximumFee()}); // U:[UTT_01]
+    }
+
+    /// @dev Returns fee rate
+    function _basisPointsRate() internal view returns (uint256) {
+        return IUSDT(usdt).basisPointsRate();
+    }
+
+    /// @dev Returns maximum absolute fee
+    function _maximumFee() internal view returns (uint256) {
+        return IUSDT(usdt).maximumFee();
     }
 }
