f8a772ce506babee07b5739c7712adbe72ae7c2b
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-07-24 17:51:16 +0300

    fix: `AddressProviderV3` cleanup

--- contracts/interfaces/IAddressProviderV3.sol
+++ contracts/interfaces/IAddressProviderV3.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.17;
 import {IVersion} from "@gearbox-protocol/core-v2/contracts/interfaces/IVersion.sol";
 
 uint256 constant NO_VERSION_CONTROL = 0;
-// Repositories & services
+
 bytes32 constant AP_CONTRACTS_REGISTER = "CONTRACTS_REGISTER";
 bytes32 constant AP_ACL = "ACL";
 bytes32 constant AP_PRICE_ORACLE = "PRICE_ORACLE";
@@ -21,12 +21,15 @@ bytes32 constant AP_ROUTER = "ROUTER";
 bytes32 constant AP_BOT_LIST = "BOT_LIST";
 bytes32 constant AP_GEAR_STAKING = "GEAR_STAKING";
 
-interface IAddressProviderEvents {
-    /// @dev Emits when an address is set for a contract role
-    event AddressSet(bytes32 indexed service, address indexed newAddress, uint256 indexed version);
+interface IAddressProviderV3Events {
+    /// @notice Emitted when an address is set for a contract key
+    event SetAddress(bytes32 indexed key, address indexed value, uint256 indexed version);
 }
 
-interface IAddressProviderV3 is IAddressProviderEvents, IVersion {
+/// @title Address provider V3 interface
+interface IAddressProviderV3 is IAddressProviderV3Events, IVersion {
+    function addresses(bytes32 key, uint256 _version) external view returns (address);
+
     function getAddressOrRevert(bytes32 key, uint256 _version) external view returns (address result);
 
     function setAddress(bytes32 key, address value, bool saveVersion) external;
