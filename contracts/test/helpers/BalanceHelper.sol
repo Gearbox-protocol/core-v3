bc1e13a58dd9ef54c20101b13db7f235bfc1b257
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-07-25 23:14:16 +0300

    chore: update licenses copyright notices
    * `contracts/` are licensed under BUSL-1.1
    * `contracts/interfaces/` are licensed under MIT
    * `contracts/tests` are unlicensed

--- contracts/test/helpers/BalanceHelper.sol
+++ contracts/test/helpers/BalanceHelper.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 // Gearbox Protocol. Generalized leverage for DeFi protocols
-// (c) Gearbox Holdings, 2022
+// (c) Gearbox Foundation, 2023.
 pragma solidity ^0.8.17;
 
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
