2cc2d7d4b9652d9ceab343fa4c0236b24b592b8b
0xmikko <26343374+0xmikko@users.noreply.github.com>
2024-03-19 11:30:02 +0100

    fix: problem with underlying in CreditManagerV3_USDT

--- contracts/credit/CreditManagerV3_USDT.sol
+++ contracts/credit/CreditManagerV3_USDT.sol
@@ -5,13 +5,14 @@ pragma solidity ^0.8.17;
 
 import {CreditManagerV3} from "./CreditManagerV3.sol";
 import {USDT_Transfer} from "../traits/USDT_Transfer.sol";
+import {IPoolV3} from "../interfaces/IPoolV3.sol";
 
 /// @title Credit manager V3 USDT
 /// @notice Credit manager variation for USDT underlying with enabled transfer fees
 contract CreditManagerV3_USDT is CreditManagerV3, USDT_Transfer {
     constructor(address _addressProvider, address _pool, string memory _name)
         CreditManagerV3(_addressProvider, _pool, _name)
-        USDT_Transfer(underlying)
+        USDT_Transfer(IPoolV3(_pool).asset())
     {}
 
     function _amountWithFee(uint256 amount) internal view override returns (uint256) {
