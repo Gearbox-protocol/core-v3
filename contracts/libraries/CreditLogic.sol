2eca2cce38631f3c8d3768438e6f81ddac1f7418
Dima Lekhovitsky <lekhovitsky@gmail.com>
2024-06-18 13:44:02 +0300

    fix: lt configuration fixes (#225)
    In this PR:
    * `CreditConfiguratorV3.setFees` now reverts if new underlying's LT is below any collateral token's LT (and final LT in case of ramp)
    * `CreditLogic.getLiquidationThreshold` now only computes `timestampRampEnd` if `block.timestamp > timestampRampStart`, which prevents an overflow bug
    * `CreditConfiguratorV3.rampLiquidationThreshold`'s NatSpec now explicitly states that if `timestampRampStart` defaults to `block.timestamp` unless it's set to the future timestamp
    * Minor stylistic cleanup

--- contracts/libraries/CreditLogic.sol
+++ contracts/libraries/CreditLogic.sol
@@ -113,14 +113,12 @@ library CreditLogic {
         view
         returns (uint16)
     {
+        if (block.timestamp <= timestampRampStart) return ltInitial; // U:[CL-5]
+
         uint40 timestampRampEnd = timestampRampStart + rampDuration;
-        if (block.timestamp <= timestampRampStart) {
-            return ltInitial; // U:[CL-5]
-        } else if (block.timestamp < timestampRampEnd) {
-            return _getRampingLiquidationThreshold(ltInitial, ltFinal, timestampRampStart, timestampRampEnd); // U:[CL-5]
-        } else {
-            return ltFinal; // U:[CL-5]
-        }
+        if (block.timestamp >= timestampRampEnd) return ltFinal; // U:[CL-5]
+
+        return _getRampingLiquidationThreshold(ltInitial, ltFinal, timestampRampStart, timestampRampEnd); // U:[CL-5]
     }
 
     /// @dev Computes the LT during the ramping process
