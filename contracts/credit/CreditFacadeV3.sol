3a863c21770ce7da854dc687fb9856070cf810fe
Dima Lekhovitsky <lekhovitsky@gmail.com>
2024-08-14 16:53:52 +0300

    fix: add comments that disabled tokens can be seized by liquidator
    Acknowledges:
    https://github.com/spearbit-audits/review-gearbox/issues/70
    https://github.com/spearbit-audits/review-gearbox/issues/79
    https://github.com/spearbit-audits/review-gearbox/issues/81
    https://github.com/spearbit-audits/review-gearbox/issues/95

--- contracts/credit/CreditFacadeV3.sol
+++ contracts/credit/CreditFacadeV3.sol
@@ -292,6 +292,9 @@ contract CreditFacadeV3 is ICreditFacadeV3, ACLNonReentrantTrait {
     /// @dev Reverts if `creditAccount` is not opened in connected credit manager
     /// @dev Reverts if account has no debt or is neither unhealthy nor expired
     /// @dev Reverts if remaining token balances increase during the multicall
+    /// @dev Liquidator can fully seize non-enabled tokens so it's highly recommended to avoid holding them.
+    ///      Since adapter calls are allowed, unclaimed rewards from integrated protocols are also at risk;
+    ///      bots can be used to claim and withdraw them.
     function liquidateCreditAccount(address creditAccount, address to, MultiCall[] calldata calls)
         external
         override
@@ -367,6 +370,8 @@ contract CreditFacadeV3 is ICreditFacadeV3, ACLNonReentrantTrait {
     /// @dev Reverts if `creditAccount` is not opened in connected credit manager
     /// @dev Reverts if account has no debt or is neither unhealthy nor expired
     /// @dev Reverts if `token` is underlying
+    /// @dev Like in full liquidations, liquidator can seize non-enabled tokens from the credit account, altough
+    ///      here they are actually used to repay debt; unclaimed rewards are also safe in this case
     function partiallyLiquidateCreditAccount(
         address creditAccount,
         address token,
