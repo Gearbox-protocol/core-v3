4b7923f57d3eb5f7d0877f75eb82a58e5a2e812a
Dima Lekhovitsky <lekhovitsky@gmail.com>
2024-09-10 16:29:23 +0300

    fix: `IBot` inherits `IVersion`

--- contracts/test/mocks/core/BotMock.sol
+++ contracts/test/mocks/core/BotMock.sol
@@ -6,6 +6,8 @@ pragma solidity ^0.8.17;
 import {IBot} from "../../../interfaces/base/IBot.sol";
 
 contract BotMock is IBot {
+    uint256 public constant override version = 3_10;
+    bytes32 public constant override contractType = "BOT_MOCK";
     uint192 public override requiredPermissions;
 
     function setRequiredPermissions(uint192 permissions) external {
